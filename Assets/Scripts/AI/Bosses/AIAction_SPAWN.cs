a4d7da9e44b316987b8d515586a388620847140c
Chestnut27 <33204695+Chestnut27@users.noreply.github.com>
2019-02-08 12:26:46 -0600

    AI Implementation Week 2: Electric Boogaloo (#5)
    * AI Group Combat Detection
    
    * Debug.Log Update
    
    Got rid of debug.log messages
    
    * Updated HealthManager
    
    Attempted to fix the respawn glitch
    
    * Added Spawn action
    
    Attempted (and failed) to add a spawn action

---
+++ Assets/Scripts/AI/Bosses/AIAction_SPAWN.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// This class is used to determine if the spawn point chosen is valid, and spawns enemies of the appropriate type
+/// </summary>
+public class AIAction_SPAWN : AIAction
+{
+    public GameObject Spawn;
+    public GameObject EnemyType;
+    private GameObject SpawnPoint;
+    
+	void Start()
+    {
+
+	}
+
+    public override bool Tick()
+    {
+       // SpawnPoint = Spawn.range();
+        if (!ActionFeasible()) return false;
+        return true;
+    }
+
+    public override bool ActionFeasible()
+    {
+        //if (SpawnPoint == validMeshPosition) return true;
+        return false;
+    }
+    
+}

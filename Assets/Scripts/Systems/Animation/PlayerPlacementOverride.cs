4d5fe0d26030da4ad0531526b1e491d69d3fb57a
Chloe <chloebrown2020@u.northwestern.edu>
2019-05-16 18:18:39 -0500

    playerplacement override

---
+++ Assets/Scripts/Systems/Animation/PlayerPlacementOverride.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlacementOverride : TimelineOverrides
+{
+    public bool MoveToThisLocation;
+    public GameObject MoveHere;
+    public GameObject Player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public override void OverrideTimeline()
+    {
+        if (MoveToThisLocation)
+        {
+            Player.transform.position = this.transform.position;
+        }
+        else
+        {
+            Player.transform.position = MoveHere.transform.position;
+        }
+    }
+}

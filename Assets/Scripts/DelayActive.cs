63a25a6057bd989afc7715ec595ba5b369c1439a
ymvrali <ymvrali@gmail.com>
2019-05-16 21:37:16 -0500

    added vanishing object and delay script

---
+++ Assets/Scripts/DelayActive.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class DelayActive : MonoBehaviour
+{
+
+    public GameObject WaitThing;
+    public float Duration;
+
+    private IEnumerable OnEnable()
+    {
+        yield return new WaitForSeconds(Duration);
+        WaitThing.SetActive(true);
+    }
+}
